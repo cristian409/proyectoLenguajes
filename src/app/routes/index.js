0e03908b962d76aa9d4f596e183ca2d9a1edacb1
pipeduque <juan10duque@gmail.com>
2022-06-05 15:55:14 -0500

    Merge branch 'master' of https://github.com/pipeduque/Gramaticas

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const helpers = require('../helpers/helpers')
-const factorGrammar = require("../controllers/LL1/FactorGrammar")
+const factorGrammar = require("../controllers/LL1/FactorGrammar");
 const leftRecursion = require("../controllers/LL1/LeftRecursion");
 const firstGrammar = require("../controllers/LL1/FirstGrammar");
+const following = require("../controllers/LL1/following");
+const productionsSet = require("../controllers/LL1/ProductionSet");
 const LR0 = require("../controllers/LR0/LR0");
-
 const { body, validationResult } = require('express-validator');
 
 router.get('/', (req, res) => {
@@ -28,15 +29,21 @@ router.post("/index",
             res.render('index.html', { validation: validation });//Se renderiza el index con la informacion de la validacion
         } else {
             const factorMap = helpers.grammarMap(grammarJson); //Se pasa el json por parametro y se recibe el objeto Mapa
-            const factorMapFac = new Map() //Objeto Mapa que se utiliza en la factorizacion
+
+            const factoring = factorGrammar.factorGrammar(factorMap);//Factorizacion
             const recursionGrammar = leftRecursion.LeftRecursion(factorMap);//Recursion izquierda
             const resFirstMap = firstGrammar.firstGrammar(recursionGrammar);//Primeros 
-            const gFactor = helpers.grammarJson(factorMapFac);//Se pasa el objeto mapa de la factorizacion
-            const gLeftRecursion = helpers.grammarJson(recursionGrammar); //Se pasa el objeto mapa de la recursion y se convierte en json 
-            const gFirtMap = helpers.grammarJson(resFirstMap);//Se pasa el objeto mapa de los primero y se convierte en json 
-            const LR0Map = LR0.lr0(factorMap, initial)
+            const resfollowMap = following.followingGrammar(recursionGrammar, resFirstMap, initial);//siguientes
+            const conjuntoGrammar = productionsSet.productionSet(recursionGrammar, resFirstMap, resfollowMap);//conjunto prediccion
 
-            res.render('index.html', { gFactor, gLeftRecursion, gFirtMap, LR0Map });
+            const gFactorGrammar = helpers.grammarJson(factoring);//Se pasa el obejto mapa de la factorizacion y se convierte en json
+            const gLeftRecursion = helpers.grammarJson(recursionGrammar); //Se pasa el objeto mapa de la recursion y se convierte en json 
+            const gFirtMap = helpers.grammarJson(resFirstMap);//Se pasa el objeto mapa de los primero y se convierte en json
+            const gFollow = helpers.grammarJson(resfollowMap);//Se pasa el objeto mapa de los siguientes y se convierte en json
+            const gConjunto = helpers.grammarJson(conjuntoGrammar);//Se pasa el objeto mapa de los conjunto prediccion y se convierte en json
+            const resLR0 = LR0.lr0(factorMap, initial)
+
+            res.render('index.html', { gFactorGrammar, gLeftRecursion, gFirtMap, gFollow, gConjunto, resLR0});//Se rederiza el index con la informacion del proyecto
         }
     });
 
